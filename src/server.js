da0858636fddd9d3a7f2e936ee3dfd5232fa94d8
Dárley Raffael <darleyraffael@gmail.com>
2023-03-09 13:53:58 -0300

    criando primeira rota

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-const app = express()
-app.use(express.json())
 
-const PORT = process.env.PORT || 3000
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("olá mundo");
+});
 
 app.listen(PORT, () => {
-	console.log(`Server running on port: ${PORT}`)
-})
+	console.log(`Server running on port: ${PORT}`);
+});
 
