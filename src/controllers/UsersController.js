e4e9409472232dc5ef0135864a4aeeee677c4692
Dárley Raffael <darleyraffael@gmail.com>
2023-03-13 08:00:11 -0300

    userController, rota cração usuário

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,24 @@
+const UserRepository = require("../repositories/UserRepository");
+const UserCreateService = require("../services/UserCreateService");
+const AppError = require("../utils/AppError");
+
+
+class UsersController {
+	async create(req, res) {
+		const userRepository = new UserRepository();
+		const userCreateService = new UserCreateService(userRepository);
+		const { name, email, password } = req.body;
+
+		await userCreateService.execute({ name, email, password });
+
+		return res.status(201).json({ status: "Criado com sucesso" });
+	}
+
+	//  Criar o controller de (Update)
+
+
+
+}
+
+
+module.exports = UsersController;
