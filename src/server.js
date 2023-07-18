f3138f66b14d92f19fb5ceda73a9d5713f8c9ca8
Dárley Raffael <darleyraffael@gmail.com>
2023-07-18 19:49:44 -0300

    config packages e deploy

--- src/server.js
+++ src/server.js
@@ -26,17 +26,19 @@ app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: "error",
-      message: error.message
+      message: error.message,
     });
-  };
+  }
 
   console.error(error);
 
   return response.status(500).json({
     status: "error",
-    message: "Internal Server Error"
+    message: "Internal Server Error",
   });
 });
 
 const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running on Port 🚀: http://localhost:${PORT}`)
+);
